44d809f76d0d769cf39ea6a4f3162b8502a4a572
Wolfmdq <139662630+Wolfmdq@users.noreply.github.com>
2023-10-03 01:29:41 -0300

    Add files via upload

---
+++ tp3_backend.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$i=1;
+print "<p>Ejercicio 1</p>\n";
+
+while($i<=100) {
+    print "<p>$i</p>\n";
+    $i++;
+ } 
+ print "<p>Ejercicio 2</p>\n";
+
+ for ($i=100; $i>=1; $i--)
+ print "<p>$i</p>\n";
+
+ print "<p>Ejercicio 3</p>\n";
+
+ for($n=2; $n<=100; $n=$n+2)
+ print "<p>$n</p>\n";
+
+ print "<p>Ejercicio 4</p>\n";
+
+ for($n=1; $n<=100; $n=$n+2)
+ print "<p>$n</p>\n";
+
+
+?>
+</body>
+</html>                                            
